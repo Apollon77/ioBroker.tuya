e8b6c728c6beab7386b88c9bc646d684b7dd6a1b
Dominic Griesel <d.griesel@gmx.net>
2019-03-10 09:16:54 +0100

    Fix unit test timeouts

--- test/unit.js
+++ test/unit.js
@@ -2,4 +2,4 @@ const path = require('path');
 const { tests } = require('@iobroker/testing');
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, '..'));
+tests.unit(path.join(__dirname, '..'), { startTimeout: 10000 });
