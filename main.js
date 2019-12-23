41c296877e39e0070280fe003d2cb94bcc805713
Ingo Fischer <github@fischer-ka.de>
2019-12-24 00:25:59 +0100

    3.1.15

--- main.js
+++ main.js
@@ -574,7 +574,7 @@ function startProxy(msg) {
         }
         if (isNaN(msg.message.proxyWebPort) || msg.message.proxyWebPort < 1024 || msg.message.proxyWebPort > 65535) {
             adapter.log.warn('Invalid port set for Proxy web port. Reset to 8889');
-            msg.message.proxyPort = 8889;
+            msg.message.proxyWebPort = 8889;
         }
 
         const dataDir = path.normalize(utils.controllerDir + '/' + require(utils.controllerDir + '/lib/tools').getDefaultDataDir());
